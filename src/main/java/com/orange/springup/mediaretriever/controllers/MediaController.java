f74c9e3647ec26ba9fea095579a3849bbb697bb6
Mihai.Poenaru <mihai.poenaru@orange.com>
2019-10-04 13:15:29 +0300

    Updated the endpoint

--- src/main/java/com/orange/springup/mediaretriever/controllers/MediaController.java
+++ src/main/java/com/orange/springup/mediaretriever/controllers/MediaController.java
@@ -1,26 +1,28 @@
 package com.orange.springup.mediaretriever.controllers;
 
 import com.orange.springup.mediaretriever.business.objects.Media;
-import com.orange.springup.mediaretriever.services.OMDBDownloader;
+import com.orange.springup.mediaretriever.mongo.MediaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
 public class MediaController {
-    private final OMDBDownloader downloader;
-
+    private final MediaRepository repo;
     @Autowired
-    public MediaController(OMDBDownloader downloader) {
-        this.downloader = downloader;
+    public MediaController(MediaRepository repo) {
+        this.repo = repo;
     }
 
     @GetMapping("/GetMedia")
-    public List<Media> getMovies() throws IOException, URISyntaxException {
-        return downloader.getAllMedia();
+    public List<Media> getMovies(@RequestParam(required = false) String title) {
+        if (title == null) {
+            return repo.findAll();
+        }
+
+        return repo.findAllByTitle(title);
     }
 }
--- src/main/java/com/orange/springup/mediaretriever/mongo/MediaRepository.java
+++ src/main/java/com/orange/springup/mediaretriever/mongo/MediaRepository.java
@@ -2,9 +2,11 @@ package com.orange.springup.mediaretriever.mongo;
 
 import com.orange.springup.mediaretriever.business.objects.Media;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface MediaRepository extends MongoRepository<Media, String> {
+    @Query(value = "{'title': {$regex: '.*?0.*', $options: 'i'}}")
     List<Media> findAllByTitle(String title);
 }
